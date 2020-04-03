c1151aefb8cfc765b3f8bcba9518b6319d587864
Danny Kaplan <kaplan@macalester.edu>
2020-04-03 15:50:13 -0500

    Moved data from MAT160 folder

---
+++ R/Defects.R
@@ -0,0 +1,21 @@
+#' Defects in eyeglass manufacturing
+#'
+#' Types of defects that an Eyeglass company sees in the lenses they
+#' make into eyeglasses.
+#'
+#' @docType data
+#'
+#' @usage data(Defects)
+#'
+#' @format A data.frame with one variable. Each row is
+#' one pair of eyeglasses manufactured by Eyeglass-omatic
+#'
+#  type: The type of defect that is Seen when making eyeglasses by Eyeglassomatic
+#'
+#' @keywords datasets
+#'
+#' @source Personal communication from John Matic,
+#' a consultant to Eyeglass-omatic in Australia. (The company’s name is fictitious, but the data is from an actual company.)
+#'
+#'
+"Defects"
---
+++ R/Eyeglasses.R
@@ -0,0 +1,21 @@
+#' Processes used in eyeglass manufacturing
+#'
+#' @docType data
+#'
+#' @usage data(Eyeglasses)
+#'
+#' @format A data.frame with one variable. Each row is
+#' one pair of eyeglasses manufactured by Eyeglass-omatic
+#'
+#' - `activity` the operation performed on the eyeglasses. For each
+#' pair of glasses, the company performeds a single operation:
+#' `Assemble`, `Grind`, put the `frames` together. `received` means
+#' the eyeglasses were already totally finished. Guess what `Unknown` means!
+#'
+#' @keywords datasets
+#'
+#' @source Personal communication from John Matic,
+#' a consultant to Eyeglass-omatic in Australia. (The company’s name is fictitious, but the data is from an actual company.)
+#'
+#'
+"Eyeglasses"
